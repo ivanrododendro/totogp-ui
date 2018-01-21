c402b65f2e258fc59060acc7d9813147ac754ae3
RODODENDRO <ivan.rododendro@atos.net>
2018-01-21 18:24:35 +0100

    Ajout primeng et font-awesome

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  items: MenuItem[];
 
   ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        items: [
+          { label: 'New', icon: 'fa-plus' },
+          { label: 'Open', icon: 'fa-download' }
+        ]
+      },
+      {
+        label: 'Edit',
+        items: [
+          { label: 'Undo', icon: 'fa-refresh' },
+          { label: 'Redo', icon: 'fa-repeat' }
+        ]
+      }
+    ];
   }
-
+  constructor() {}
 }
