206b574c3e017e3139c68bc815581ae40d7a704e
RODODENDRO <ivan.rododendro@atos.net>
2018-01-21 17:54:42 +0100

    Configuration bootstrap et ajout des composants home et shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedComponent } from './shared/shared.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule
