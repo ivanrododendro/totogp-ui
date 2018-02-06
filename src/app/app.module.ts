aa8331af7502f35a85ae328b93bb8ab8bc670a75
Ivan <ivan.rododendro@gmail.com>
2018-02-06 21:40:37 +0100

    Added home bet root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { RootComponent } from './root/root.component';
+import { HomeComponent } from './home/home.component';
+import { BetComponent } from './bet/bet.component';
+import { DriverSelectorComponent } from './bet/driver-selector/driver-selector.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RootComponent, HomeComponent, BetComponent, DriverSelectorComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
